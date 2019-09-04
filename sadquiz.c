c4fb49580136ecc0a69bed4041e2872dc8a71add
iana08 <iana.dustydepot@gmail.com>
2019-09-04 07:03:21 +0100

    Goood Morning

--- sadquiz.c
+++ sadquiz.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 int main(int argc, char * argv[])
 {
-	int fd_in;
-	int count;
-	char buf[1];
+	FILE * fd_in;
 	int count_this = 0;
+	char ch[128];
 
-	fd_in = open(argv[1], O_RDONLY);
-	if(fd_in < 0)
+	fd_in = fopen(argv[1], "r");
+	if(fd_in == NULL)
 	{
 		printf("Can't open.... and where did it lead you?... \n");
 		printf("Right back here to me\n");
 		printf("*SNAP*\n");
 		exit(1);
 	}
-	
-	while((count = read(fd_in, buf, 1)) > 0)
+	while(fgets(ch, 128, fd_in) != NULL)
 	{
-		count_this++;
+		if(*ch == '\n')
+		{	
+			count_this++;
+		}
+		
 	}
 
-	close(fd_in);
+	fclose(fd_in);
 
 	printf("What will this be?: %d\n", count_this);
+	return 0;
 }
